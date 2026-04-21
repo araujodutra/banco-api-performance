5147a16a8016781142db66edf65653304db0b915
unknown <araujoantoniojsd@gmail.com>
2026-04-21 01:02:08 -0300

    Criei um arquivo de configuração e apliquei variáveis de ambiente

--- tests/transferencias.test.js
+++ tests/transferencias.test.js
@@ -1,35 +1,38 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 import { obterToken } from '../helpers/autenticacao.js';
+import { pegarBaseURL } from '../utils/variaveis.js';
 
 export const options = {
   iterations: 1
 };
 
-export default function() {
-  const token = obterToken ()
 
-  const url = 'http://localhost:3000/transferencias';
 
-  const payload = JSON.stringify(postLogin);({
+export default function () {
+  const token = obterToken();
+
+  const url = pegarBaseURL() + '/transferencias';
+
+  const payload = JSON.stringify({
     contaOrigem: 1,
     contaDestino: 2,
     valor: 11,
-    token: ""
+    
   });
 
   const params = {
-     headers: {
+    headers: {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer ' + token
     },
   };
 
-  let res =http.post(url, payload, params)
-  
-  check(res, { 
-    "status is 201": (res) => res.status === 201
-   });
+  const res = http.post(url, payload, params);
+
+  check(res, {
+    'status is 201': (r) => r.status === 201
+  });
 
   sleep(1);
 }
